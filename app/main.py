b5e4c9cbc563d067351be7c047a23bcf7c594fb2
Shazinabbas <shazinabbas_m@srmap.edu.in>
2024-11-11 12:17:38 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,8 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, addr = server_socket.accept()  # wait for client
-    client.send(b"+PONG\r\n")
-    client.send(b"+PONG\r\n")
+    client.send(b"+PONGPONG\r\n")
 
 
 if __name__ == "__main__":
