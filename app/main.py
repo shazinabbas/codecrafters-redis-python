771676fbe005062a3fa6658e83744ea7877b0416
Shazinabbas <shazinabbas_m@srmap.edu.in>
2024-11-11 12:10:35 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,9 +9,8 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, addr = server_socket.accept()  # wait for client
-    client.send(b"+PONG\r\n")
-    client.send(b"+PONG\r\n")
-
+    if "ping" in data.lower():
+            client_socket.send("+PONG\r\n".encode())
 
 if __name__ == "__main__":
     main()
