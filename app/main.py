d0b93064bcefe31acef62ee49bb1aecd1e35cd89
Shazinabbas <shazinabbas_m@srmap.edu.in>
2024-11-11 12:27:27 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,25 +1,27 @@
 import socket
-
-def main():
-    print("Logs from your program will appear here!")
-
-    # Create a server socket that listens on localhost, port 6379
+import threading
+def main() -> None:
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, addr = server_socket.accept()  # Wait for a client to connect
-    print(f"Connected by {addr}")
-
-    try:
-        while True:
-            data = client.recv(1024)  # Receive data from client
-            if not data:
-                break  # Exit the loop if there's no more data (client disconnected)
-            print(f"Received data: {data}")
-
-            # Send a response back for each command received
-            client.sendall(b"+PONG\r\n")
-    finally:
-        client.close()  # Close the client connection after loop exits
-        server_socket.close()  # Close the server socket when done
-
+    while True:
+        connection: socket.socket
+        address: tuple[str, int]
+        connection, address = server_socket.accept()
+        print(f"accepted connection - {address[0]}:{str(address[1])}")
+        connect(connection)
+        thread: threading.Thread = threading.Thread(target=connect, args=[connection])
+        thread.start()
+def connect(connection: socket.socket) -> None:
+    with connection:
+        connected: bool = True
+        while connected:
+            command: str = connection.recv(1024).decode()
+            print(f"recieved - {command}")
+            connected = bool(command)
+            response: str
+            match command:
+                case "*1\r\n$4\r\nPING\r\n":
+                    response = "+PONG\r\n"
+            print(f"responding with - {response}")
+            connection.sendall(response.encode())
 if __name__ == "__main__":
     main()
