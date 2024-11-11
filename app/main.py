38dbbf995e10a6f711112e229c8be9358bf135fd
Shazinabbas <shazinabbas_m@srmap.edu.in>
2024-11-11 12:18:43 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -2,14 +2,17 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, addr = server_socket.accept()  # wait for client
-    client.send(b"+PONG\r\n")
+
+    while True:
+        data = client.recv(1024)  # receive data from client
+        if not data:
+            break
+        if data.strip() == b"PING":
+            client.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
